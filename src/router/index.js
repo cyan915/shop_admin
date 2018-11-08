1c8decfe119a54e8a2b2caaeba1bbbe138749864
Cyan <349631091@qq.com>
2018-11-08 17:10:08 +0800

    项目初始化提交

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+
+import Router from 'vue-router'
+
+// 导入Login组件
+import Login from '@/components/Login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/',
+      component: Login
+    }
+  ]
+})
